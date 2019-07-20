e8b2ea701c8789ed9ba583fc6000763e4cf55e6a
Anthrapper <43750775+Anthrapper@users.noreply.github.com>
2019-07-20 10:28:31 +0530

    Create credentials.py

---
+++ modules/credentials.py
@@ -0,0 +1,6 @@
+class Creds(object):
+    TOKEN = "982503685:AAHtkp0p-XMtHuU3fEsr55YP_2T9aa2qWpo"
+    CLIENT_ID = "604374977160-25iq5tv4b9lccmq9e0o5vce4jng259hp.apps.googleusercontent.com"
+    CLIENT_SECRET = "I9zb6LsGfvEDDAU--Hm9WmXz"
+    PATH_ID = "1_mboCE-iUqjIZ3FQk0F-Pqp4ejEkOQWi"
+    USER_ID = "673742688"
